9f057f0cb30520af38d7e7f1786e982aafeae1be
Tony Bonki <tony.bonki2@mail.dcu.ie>
2023-08-05 12:17:05 +0100

    updated footer

--- react-todo-list/src/components/Footer.jsx
+++ react-todo-list/src/components/Footer.jsx
@@ -47,7 +47,7 @@ export default function Footer() {
           </Link>
 
           <Link fontSize={"14px"} href="https://app.brandmark.io/v3/">
-            React logo made by App.brandmark.ie
+            React logo made on App.brandmark.ie
           </Link>
         </Box>
       </Container>
