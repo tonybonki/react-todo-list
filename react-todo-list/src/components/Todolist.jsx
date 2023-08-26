75ddf6b36dd2288d5666a0f23ad2b3ef3017f76c
Tony Bonki <tony.bonki2@mail.dcu.ie>
2023-08-26 11:32:29 +0100

    Cleaned up app.jsx file created tdl .jsx

--- react-todo-list/src/components/Todolist.jsx
+++ react-todo-list/src/components/Todolist.jsx
@@ -1,19 +1,24 @@
-import { TodoItem } from "./TodoItem"
+import React from "react";
+import { useTodoManager } from "./functions/useTodoManager"; // Import the custom hook
+import { TodoItem } from "./TodoItem"; // Import TodoItem component if needed
 
-export function TodoList({ todos, toggleTodo, deleteTodo }) {
+export function TodoList() {
+  const { todos, addTodo, toggleTodo, deleteTodo } = useTodoManager(); // Use the custom hook
+
   return (
-    <ul className="list">
-      {todos.length === 0 && "No Todos"}
-      {todos.map(todo => {
-        return (
+    <div>
+      <button onClick={() => addTodo("New Todo")}>Add Todo</button>
+      <ul className="list">
+        {todos.length === 0 && <p>No Todos</p>}
+        {todos.map((todo) => (
           <TodoItem
             {...todo}
             key={todo.id}
             toggleTodo={toggleTodo}
             deleteTodo={deleteTodo}
           />
-        )
-      })}
-    </ul>
-  )
+        ))}
+      </ul>
+    </div>
+  );
 }
---
+++ react-todo-list/src/components/functions/useTodoManager.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+export function useTodoManager() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(title) {
+    setTodos((currentTodos) => [
+      ...currentTodos,
+      { id: crypto.randomUUID(), title, completed: false },
+    ]);
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) =>
+      currentTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed } : todo
+      )
+    );
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => currentTodos.filter((todo) => todo.id !== id));
+  }
+
+  return { todos, addTodo, toggleTodo, deleteTodo };
+}
