61088651888f88e67feddb09a1d7569286c8c2fa
Tony Bonki <tony.bonki2@mail.dcu.ie>
2023-08-28 11:30:16 +0100

    added text fade when checkbox is clicked

--- react-todo-list/src/components/TodoItem.jsx
+++ react-todo-list/src/components/TodoItem.jsx
@@ -1,4 +1,14 @@
-import { Checkbox, Button, ButtonGroup, IconButton, Heading, Text, Box, HStack } from "@chakra-ui/react";
+import {
+  Checkbox,
+  Button,
+  ButtonGroup,
+  IconButton,
+  Heading,
+  Text,
+  Box,
+  extendTheme,
+  HStack,
+} from "@chakra-ui/react";
 
 import DeleteIcon from "./Icons/DeleteIcon";
 export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
@@ -9,24 +19,24 @@ export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
         onChange={(e) => toggleTodo(id, e.target.checked)}
       />
       <Box my={2}>
-        <Text fontWeight={"light"} fontSize="sm">
+        <Text
+          style={{
+            opacity: completed ? 0.5 : 1,
+            transition: "opacity 0.2s ease-out", // Add the transition effect
+          }}
+          fontWeight={"light"}
+          fontSize="sm"
+        >
           {title}
         </Text>
       </Box>
 
       <ButtonGroup size="xs" isAttached variant="outline">
-        <Button
-          onClick={() => deleteTodo(id)}
-          className="btn btn-danger"
-          fontWeight={"light"}
-        >
+        <Button onClick={() => deleteTodo(id)} fontWeight={"light"}>
           Delete
         </Button>
 
-        <IconButton
-          onClick={() => deleteTodo(id)}
-          icon={<DeleteIcon />}
-        />
+        <IconButton onClick={() => deleteTodo(id)} icon={<DeleteIcon />} />
       </ButtonGroup>
     </HStack>
   );
